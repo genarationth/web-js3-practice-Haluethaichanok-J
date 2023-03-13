279a23d9c1f32b341c0b61aee35675388a824fc6
Praew <114122183+Haluethaichanok-J@users.noreply.github.com>
2023-03-13 16:32:14 +0700

    add file

---
+++ Assignment1.js
@@ -0,0 +1,30 @@
+let sum ;
+function addNumber(a,b){
+sum=a+b;
+console.log(sum);
+}
+addNumber(4,5);
+
+function subtractNum(a,b){
+    sum = a-b;
+    console.log(sum);
+}
+subtractNum(3,2);
+
+function multiplyNum(a,b){
+    sum = a*b;
+    console.log(sum);
+}
+multiplyNum(3,2);
+
+function devideNum(a,b){
+    sum = a/b;
+    console.log(sum);
+}
+devideNum(6,2);
+
+
+function inform(firstName, location, hobby){
+    console.log("Hi, my name is " + firstName + " I live in " + location + " and enjoy "+ hobby+".");
+}
+inform('John', 'Korat','drawing');
